05e6f1db9e9df430ea7029106d792ae2ac2a680c
marcochavezco <machavezco@gmail.com>
2025-02-20 21:35:15 -0600

    implement top half and bottom half droppable zone

--- components/Designer.tsx
+++ components/Designer.tsx
@@ -78,8 +78,40 @@ function Designer() {
 export default Designer;
 
 function DesignerElementWrapper({ element }: { element: FormElementInstance }) {
+  const topHalf = useDroppable({
+    id: `${element.id}-top`,
+    data: {
+      type: element.type,
+      elementId: element.id,
+      isTopHalfDesignerElement: true,
+    },
+  });
+
+  const bottomHalf = useDroppable({
+    id: `${element.id}-bottom`,
+    data: {
+      type: element.type,
+      elementId: element.id,
+      isBottomHalfDesignerElement: true,
+    },
+  });
+
   const DesignerElement =
     FormElements[element.type as ElementsType].designerComponent;
 
-  return <DesignerElement elementInstance={element} />;
+  return (
+    <div className='relative h-[120px] flex flex-col text-foreground hover:cursor-pointer rounded-md ring-1 ring-accent ring-inset'>
+      <div
+        ref={topHalf.setNodeRef}
+        className='absolute w-full h-1/2 rounded-t-md'
+      ></div>
+      <div
+        ref={bottomHalf.setNodeRef}
+        className='absolute bottom-0 w-full h-1/2 rounded-b-md'
+      ></div>
+      <div className='flex w-full h-[120px] items-center rounded-md bg-accent/40 px-4 py-2 pointer-events-none'>
+        <DesignerElement elementInstance={element} />
+      </div>
+    </div>
+  );
 }
