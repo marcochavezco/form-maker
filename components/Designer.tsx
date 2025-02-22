182a18abb525ae1a1f120ed640c746669e6228fc
marcochavezco <machavezco@gmail.com>
2025-02-21 20:04:00 -0600

    Set DesignerElementComponent draggable functionality

--- components/Designer.tsx
+++ components/Designer.tsx
@@ -1,8 +1,13 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import DesignerSidebar from './DesignerSidebar';
-import { DragEndEvent, useDndMonitor, useDroppable } from '@dnd-kit/core';
+import {
+  DragEndEvent,
+  useDndMonitor,
+  useDraggable,
+  useDroppable,
+} from '@dnd-kit/core';
 import { cn } from '@/lib/utils';
 import useDesigner from './hooks/useDesigner';
 import {
@@ -11,6 +16,8 @@ import {
   FormElements,
 } from './FormElements';
 import { idGenerator } from '@/lib/idGenerator';
+import { Button } from './ui/button';
+import { TrashIcon } from 'lucide-react';
 
 function Designer() {
   const { elements, addElement } = useDesigner();
@@ -56,7 +63,7 @@ function Designer() {
               Drop here
             </p>
           )}
-          {droppable.isOver && (
+          {droppable.isOver && elements.length === 0 && (
             <div className='p-4 w-full'>
               <div className='h-[120px] rounded-md bg-primary/20'></div>
             </div>
@@ -78,6 +85,10 @@ function Designer() {
 export default Designer;
 
 function DesignerElementWrapper({ element }: { element: FormElementInstance }) {
+  const { removeElement } = useDesigner();
+
+  const [mouseIsOver, setMouseIsOver] = useState<boolean>(false);
+
   const topHalf = useDroppable({
     id: `${element.id}-top`,
     data: {
@@ -96,22 +107,71 @@ function DesignerElementWrapper({ element }: { element: FormElementInstance }) {
     },
   });
 
+  const draggable = useDraggable({
+    id: `${element.id}-drag-handler`,
+    data: {
+      type: element.type,
+      elementId: element.id,
+      isDesignerElement: true,
+    },
+  });
+
+  if (draggable.isDragging) {
+    return null;
+  }
+
   const DesignerElement =
     FormElements[element.type as ElementsType].designerComponent;
 
   return (
-    <div className='relative h-[120px] flex flex-col text-foreground hover:cursor-pointer rounded-md ring-1 ring-accent ring-inset'>
+    <div
+      ref={draggable.setNodeRef}
+      {...draggable.listeners}
+      {...draggable.attributes}
+      className='relative h-[120px] flex flex-col text-foreground hover:cursor-pointer rounded-md ring-1 ring-accent ring-inset'
+      onMouseEnter={() => setMouseIsOver(true)}
+      onMouseLeave={() => setMouseIsOver(false)}
+    >
       <div
         ref={topHalf.setNodeRef}
         className='absolute w-full h-1/2 rounded-t-md'
-      ></div>
+      />
       <div
         ref={bottomHalf.setNodeRef}
         className='absolute bottom-0 w-full h-1/2 rounded-b-md'
-      ></div>
-      <div className='flex w-full h-[120px] items-center rounded-md bg-accent/40 px-4 py-2 pointer-events-none'>
+      />
+      {mouseIsOver && (
+        <>
+          <div className='absolute right-0 h-full'>
+            <Button
+              className='flex justify-center h-full border rounded-md rounded-l-none bg-red-700'
+              variant={'outline'}
+              onClick={() => removeElement(element.id)}
+            >
+              <TrashIcon className='h-6 w-6' />
+            </Button>
+          </div>
+          <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 animate-pulse'>
+            <p className='text-muted-foreground text-sm'>
+              Click for propperties or drag to move
+            </p>
+          </div>
+        </>
+      )}
+      {topHalf.isOver && (
+        <div className='absolute top-0 w-full rounded-md h-[7px] bg-primary rounded-b-none' />
+      )}
+      <div
+        className={cn(
+          'flex w-full h-[120px] items-center rounded-md bg-accent/40 px-4 py-2 pointer-events-none opacity-100',
+          mouseIsOver && 'opacity-30'
+        )}
+      >
         <DesignerElement elementInstance={element} />
       </div>
+      {bottomHalf.isOver && (
+        <div className='absolute bottom-0 w-full rounded-md h-[7px] bg-primary rounded-t-none' />
+      )}
     </div>
   );
 }
